2ebe2e80f1bbc1f43954a5d5c62b8e6777d4d40d
tomi <tomimateo2008@hormail.com>
2026-01-20 17:56:46 -0300

    chore: clean up next files before merge

--- mul-front/.next/dev/static/chunks/app_page_tsx_7c3c084c._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_dist_60d9f176._.js",
-  "static/chunks/app_7f3df4dc._.js"
-],
-    source: "dynamic"
-});
